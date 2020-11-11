8cacb5fdbe5a4b9353e4557d8296ae35221015c3
Jonny K <Kempj2.jk@gmail.com>
2020-11-11 12:20:17 -0800

    removing uneeded routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,10 +9,6 @@ export default {
    getGBooks: function(title) {
     return axios.get("/api/books/gb/" + title);
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
   // Deletes the book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
--- controllers/booksController.js
+++ controllers/booksController.js
@@ -20,28 +20,11 @@ module.exports = {
       // handle error
       console.log(error);
     })
-    // db.Book
-    //   .find(req.query)
-    //   .sort({ date: -1 })
-    //   .then(dbModel => res.json(dbModel))
-    //   .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Book
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     console.log(req.body)
     db.Book
       .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
