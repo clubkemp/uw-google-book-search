b714daf2ad78382a9687aa96ea635409f2f234eb
Jonny K <Kempj2.jk@gmail.com>
2020-11-11 17:25:01 -0800

    saved component functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero"
 
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,24 +1,32 @@
-import React from "react";
-const [books, setBooks] = useState([]);
-useEffect(() => {
-  loadBooks();
-}, []);
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import Bookcard from "../components/Bookcard";
+import { Button}  from 'react-bulma-components';
 
-function loadBooks() {
-  API.getBooks()
-    .then((res) => setBooks(res.data))
-    .catch((err) => console.log(err));
-}
-function handleDeleteBook(id) {
-  API.deleteBook(id)
-    .then((res) => loadBooks())
-    .catch((err) => console.log(err));
-}
 
 function Saved() {
-  return (
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+    loadBooks();
+    }, []);
+
+    function loadBooks() {
+    API.getBooks()
+        .then((res) => setBooks(res.data))
+        .catch((err) => console.log(err));
+    }
+    function handleDeleteBook(id) {
+    API.deleteBook(id)
+        .then((res) => loadBooks())
+        .catch((err) => console.log(err));
+    }
+    function saveOrDeleteBtn (book){
+        return <Button color="warning"onClick={e=>handleDeleteBook(book.g_id)}>unsave book</Button>
+    }
+    return (
     <div>
-      {googleBooks.map((book) => {
+      {books.map((book) => {
         // need to catch if any books don't have an other
         return <Bookcard data={book} btn={saveOrDeleteBtn} />;
       })}
